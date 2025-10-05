6a567ff8cae67d0614e261a83205bdd48914f6e3
Denzel <denzelhooke@hotmail.com>
2025-10-05 14:31:47 -0700

    Added modules

---
+++ backend/src/modules/health/route.ts
@@ -0,0 +1,10 @@
+import { FastifyInstance, FastifyPluginOptions } from "fastify"
+
+async function route(fastify: FastifyInstance, options: FastifyPluginOptions) {
+    fastify.get("/health", async (req, res) => {
+          return {health: "100%"}
+    })
+  
+}
+
+export default route
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,13 +1,25 @@
 // Require the framework and instantiate it
 
+// Packages
 import Fastify from 'fastify'
-import HelloWorldRoute from "./routes/helloworldroute.js"
+import cors from "@fastify/cors"
+
+//? Must change library to @fastify..? DONE
+import rateLimit from '@fastify/rate-limit'
+import jwt from '@fastify/jwt'
+
+// Routes
+import HelloWorldRoute from "./modules/routes/helloworldroute.js"
+import HealthRoute from "./modules/health/route.js"
 
 const fastify = Fastify({
   logger: true
 })
 
+fastify.register(cors, {origin: true})
+fastify.register(rateLimit, {max: 9999999, timeWindow: "1 minute"})
 fastify.register(HelloWorldRoute)
+fastify.register(HealthRoute)
                 
 // Run the server!
 fastify.listen({ port: 3000 }, function (err, address) {
@@ -16,4 +28,4 @@ fastify.listen({ port: 3000 }, function (err, address) {
     process.exit(1)
   }
   // Server is now listening on ${address}
-})
+})  
