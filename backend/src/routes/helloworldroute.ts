1da668cc089c50aec9a515707b0d283941dd3743
Denzel <denzelhooke@hotmail.com>
2025-10-05 13:53:58 -0700

    Added nodemon

--- backend/src/routes/helloworldroute.ts
+++ backend/src/routes/helloworldroute.ts
@@ -2,7 +2,7 @@ import { FastifyInstance, FastifyPluginOptions } from "fastify"
 
 async function routes (fastify: FastifyInstance, options: FastifyPluginOptions) {
     fastify.get("/", async (req, res) => {
-        return {msg: "what is up!"}
+        return {msg: "Hello World"}
     })
 }
 
